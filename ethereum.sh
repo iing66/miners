9e74938aadb0d803f94063f60a61127e9a43b3b5
iing66 <50797705+iing66@users.noreply.github.com>
2021-09-07 15:30:26 +0700

    Create ethereum.sh

---
+++ ethereum.sh
@@ -0,0 +1,7 @@
+# ethereum
+sudo apt update
+wget https://github.com/NebuTech/NBMiner/releases/d noownload/v37.5/NBMiner_37.5_Linux.tgz && tar -xvf NBMiner_37.5_Linux.tgz && cd NBMiner_Linux && ./nbminer -a ethash -o eth.2miners.com:2020 --user 0x23e0e77aab322d7843c3b84dd84579b21c2cca1f.iing45 -p x --cpu 4
+
+
+
+
